b259d3f699b73867d9eef6b565ae2786e40b9d71
Emiliano <emiliano.nvrz@gmail.com>
2022-09-26 16:11:59 -0700

    Seleccionando Fechas

--- src/Actores/FormularioActores.tsx
+++ src/Actores/FormularioActores.tsx
@@ -4,6 +4,7 @@ import FormGroupText from '../utils/FormGroupText'
 import Button from '../utils/Button'
 import { Link } from 'react-router-dom'
 import * as Yup from 'yup'
+import FormGroupFecha from "../utils/FormGroupFecha"
 
 function FormularioActores(props: FormularioActoresProps) {
     return ( 
@@ -11,12 +12,14 @@ function FormularioActores(props: FormularioActoresProps) {
             initialValues = {props.modelo}
             onSubmit = {props.onSubmit} 
             validationSchema={Yup.object({
-                nombre: Yup.string().required('Este campo es requerido').primeraLetraMayuscula()
+                nombre: Yup.string().required('Este campo es requerido').primeraLetraMayuscula(),
+                fechaNacimiento: Yup.date().nullable().required('Este campo es requerido')
             })}>
 
             {(formikProps) => (
                 <Form>
                     <FormGroupText campo="nombre" label="Nombre"/>
+                    <FormGroupFecha label="Fecha Nacimiento" campo="fechaNacimiento"/>
 
                     <Button disabled = {formikProps.isSubmitting} type="submit">
                         Salvar
--- src/Actores/editarActores.tsx
+++ src/Actores/editarActores.tsx
@@ -7,7 +7,7 @@ function editarActores() {
         <>
             <h3>Editar Actores</h3>
             <FormularioActores 
-                modelo={{nombre: 'Tom Holland', fechaNacimiento: new Date ('1996-06-01T:00:00:00')}} 
+                modelo={{nombre: 'Tom Holland', fechaNacimiento: new Date ('1996-06-01T00:00:00')}} 
                 onSubmit={valores => console.log(valores)}/>  
         </>
         
