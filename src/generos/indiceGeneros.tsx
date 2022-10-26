cf25a33bd9b9f098d774b2b0f3548875644feab9
Emiliano <emiliano.nvrz@gmail.com>
2022-10-26 16:53:55 -0700

    Comunicacion entre WebAPI y la aplicacion de REACT

--- src/generos/indiceGeneros.tsx
+++ src/generos/indiceGeneros.tsx
@@ -1,6 +1,19 @@
 import { Link } from "react-router-dom"
+import { useEffect } from 'react'
+import axios, { AxiosResponse } from "axios"
+import { generoDTO } from "./Generos.model"
+import { urlGeneros } from "../utils/endPoints"
 
 function IndiceGeneros() {
+
+    useEffect( ()=> {
+        // hacer una peticion http get hacia nuestro webapi con la libreria axios
+        axios.get(urlGeneros)
+            .then((respuesta: AxiosResponse<generoDTO[]>) => {
+                console.log(respuesta.data);
+            })
+    }, [])
+
     return ( 
         <>
             <h3>Indice Generos</h3>
