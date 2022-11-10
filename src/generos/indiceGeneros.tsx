a1601ca378f05849a6b03d29710fd77b39d5bd4d
Emiliano <emiliano.nvrz@gmail.com>
2022-11-10 09:17:38 -0700

    Paginacion en React

--- src/generos/indiceGeneros.tsx
+++ src/generos/indiceGeneros.tsx
@@ -4,25 +4,51 @@ import { Link } from "react-router-dom"
 import Button from "../utils/Button"
 import { urlGeneros } from "../utils/endPoints"
 import ListadoGenerico from "../utils/ListadoGenerico"
+import Paginacion from "../utils/Paginacion"
 import { generoDTO } from "./Generos.model"
 
 function IndiceGeneros() {
 
     const [generos, setGeneros] = useState<generoDTO[]>();
+    const [totalPaginas, setTotalPaginas] = useState(0);
+    const [recordsPorPagina, setRecordsPorPagina] = useState(5);
+    const [pagina, setPagina] = useState(1);
 
     useEffect( ()=> {
         // hacer una peticion http get hacia nuestro webapi con la libreria axios
-        axios.get(urlGeneros)
+        axios.get(urlGeneros, {
+            params: {pagina, recordsPorPagina}
+        })
             .then((respuesta: AxiosResponse<generoDTO[]>) => {
+                const totalRegistro = 
+                parseInt(respuesta.headers['cantidadtotalregistros'], 10);
+                setTotalPaginas(Math.ceil(totalRegistro/recordsPorPagina));
                 console.log(respuesta.data);
                 setGeneros(respuesta.data);
             })
-    }, [])
+    }, [pagina, recordsPorPagina])
 
     return ( 
         <>
             <h3>Generos</h3>
             <Link className="btn btn-primary" to="generos/crear">Crear Genero</Link>
+
+            <div className="form-group" style={{width: '150px'}}>
+                <label>Registros por Pagina:</label>
+                <select defaultValue={10}
+                        onChange={e => {
+                        setPagina(1);
+                        setRecordsPorPagina(parseInt(e.currentTarget.value, 10))}} className="form-control">
+                    <option value={5}>5</option>
+                    <option value={10}>10</option>
+                    <option value={25}>25</option>
+                    <option value={50}>50</option>
+                </select>
+            </div>
+
+            <Paginacion cantidadTotalPaginas={totalPaginas} 
+                        paginaActual={pagina} 
+                        onChange={nuevaPagina => setPagina(nuevaPagina)} />
 
             <ListadoGenerico listado={generos}>
                 <table className="table table-stripped">
