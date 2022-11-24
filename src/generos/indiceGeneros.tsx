17f28edba0cbf056c42c907f351afbc1ae3405df
Emiliano <emiliano.nvrz@gmail.com>
2022-11-23 18:18:28 -0700

    Generalizando el Componente de Indice

--- src/generos/indiceGeneros.tsx
+++ src/generos/indiceGeneros.tsx
@@ -1,99 +1,40 @@
-import axios, { AxiosResponse } from "axios"
-import { useEffect, useState } from 'react'
-import { Link } from "react-router-dom"
-import Button from "../utils/Button"
-import confirmar from "../utils/Confirmar"
 import { urlGeneros } from "../utils/endPoints"
-import ListadoGenerico from "../utils/ListadoGenerico"
-import Paginacion from "../utils/Paginacion"
+import IndiceEntidad from "../utils/IndiceEntidad"
 import { generoDTO } from "./Generos.model"
 
 function IndiceGeneros() {
 
-    const [generos, setGeneros] = useState<generoDTO[]>();
-    const [totalPaginas, setTotalPaginas] = useState(0);
-    const [recordsPorPagina, setRecordsPorPagina] = useState(5);
-    const [pagina, setPagina] = useState(1);
-
-    useEffect( ()=> {
-        // hacer una peticion http get hacia nuestro webapi con la libreria axios
-        cargarDatos();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [pagina, recordsPorPagina])
-
-    function cargarDatos(){
-        axios.get(urlGeneros, {
-            params: {pagina, recordsPorPagina}
-        })
-            .then((respuesta: AxiosResponse<generoDTO[]>) => {
-                const totalRegistro = 
-                parseInt(respuesta.headers['cantidadtotalregistros'], 10);
-                setTotalPaginas(Math.ceil(totalRegistro/recordsPorPagina));
-                console.log(respuesta.data);
-                setGeneros(respuesta.data);
-            })
-    }
-
-    async function borrar(id:number){
-
-        try{
-            await axios.delete(`${urlGeneros}/${id}`)
-            cargarDatos();
-        } catch(error){
-            console.log(error.response.data);
-        }
-    }
-
     return ( 
-        <>
-            <h3>Generos</h3>
-            <Link className="btn btn-primary" to="generos/crear">Crear Genero</Link>
-
-            <div className="form-group" style={{width: '150px'}}>
-                <label>Registros por Pagina:</label>
-                <select defaultValue={10}
-                        onChange={e => {
-                        setPagina(1);
-                        setRecordsPorPagina(parseInt(e.currentTarget.value, 10))}} className="form-control">
-                    <option value={5}>5</option>
-                    <option value={10}>10</option>
-                    <option value={25}>25</option>
-                    <option value={50}>50</option>
-                </select>
-            </div>
-
-            <Paginacion cantidadTotalPaginas={totalPaginas} 
-                        paginaActual={pagina} 
-                        onChange={nuevaPagina => setPagina(nuevaPagina)} />
-
-            <ListadoGenerico listado={generos}>
-                <table className="table table-stripped">
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Nombre</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {generos?.map(generos => 
-                        <tr key={generos.id}>
-                            <td>
-                                <Link className="btn btn-success" to={`/generos/editar/${generos.id}`}>
-                                   Editar 
-                                </Link>
-
-                                <Button 
-                                onClick={() => confirmar(() => borrar(generos.id))}
-                                className="btn btn-danger">Borrar</Button>
-                            </td>
-                            <td>
-                                {generos.nombre}
-                            </td>
-                        </tr>)}
-                    </tbody>
-                </table>
-            </ListadoGenerico>
-
+        <>        
+                <IndiceEntidad<generoDTO>
+                    url={urlGeneros}
+                    urlCreacion="generos/crear"
+                    titulo = "Generos"
+                    nombreEntidad = "Genero" 
+                    >
+                        {(generos, botones) => <>
+                            <thead>
+                            <tr>
+                                <th></th>
+                                <th>Nombre</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {generos?.map(generos => 
+                            <tr key={generos.id}>
+                                <td>
+                                    {botones(`generos/editar/${generos.id}`, generos.id)}
+                                </td>
+                                <td>
+                                    {generos.nombre}
+                                </td>
+                            </tr>)}
+                        </tbody>
+                        </>}
+                        
+                
+                </IndiceEntidad> 
+                   
         </>
         
      );
---
+++ src/utils/IndiceEntidad.tsx
@@ -0,0 +1,99 @@
+import axios, { AxiosResponse } from 'axios'
+import { ReactElement, useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { generoDTO } from '../Generos/Generos.model'
+import Button from './Button'
+import confirmar from './Confirmar'
+import { urlGeneros } from './endPoints'
+import ListadoGenerico from './ListadoGenerico'
+import Paginacion from './Paginacion'
+function IndiceEntidad<T>(props: indiceEntidadProps<T>) {
+
+        const [entidades, setEntidades] = useState<T[]>();
+        const [totalPaginas, setTotalPaginas] = useState(0);
+        const [recordsPorPagina, setRecordsPorPagina] = useState(5);
+        const [pagina, setPagina] = useState(1);
+    
+        useEffect( ()=> {
+            // hacer una peticion http get hacia nuestro webapi con la libreria axios
+            cargarDatos();
+            // eslint-disable-next-line react-hooks/exhaustive-deps
+        }, [pagina, recordsPorPagina])
+    
+        function cargarDatos(){
+            axios.get(urlGeneros, {
+                params: {pagina, recordsPorPagina}
+            })
+                .then((respuesta: AxiosResponse<T[]>) => {
+                    const totalRegistro = 
+                    parseInt(respuesta.headers['cantidadtotalregistros'], 10);
+                    setTotalPaginas(Math.ceil(totalRegistro/recordsPorPagina));
+                    setEntidades(respuesta.data);
+                })
+        }
+    
+        async function borrar(id:number){
+    
+            try{
+                await axios.delete(`${props.url}/${id}`)
+                cargarDatos();
+            } catch(error){
+                console.log(error.response.data);
+            }
+        }
+    
+        const botones = (urlEditar: string, id: number) => <>
+        <Link className="btn btn-success" to={urlEditar}>Editar</Link>
+        <Button onClick={() => confirmar(() => borrar(id))}
+            className="btn btn-danger">
+                Borrar
+        </Button>
+        </>
+
+    return ( 
+        <>
+            <h3>{props.titulo}</h3>
+            <Link className="btn btn-primary" to={props.urlCreacion}>
+                {props.nombreEntidad}
+            </Link>
+
+            <div className="form-group" style={{width: '150px'}}>
+                <label>Registros por Pagina:</label>
+                <select defaultValue={10}
+                        onChange={e => {
+                        setPagina(1);
+                        setRecordsPorPagina(parseInt(e.currentTarget.value, 10))}} className="form-control">
+                    <option value={5}>5</option>
+                    <option value={10}>10</option>
+                    <option value={25}>25</option>
+                    <option value={50}>50</option>
+                </select>
+            </div>
+
+            <Paginacion cantidadTotalPaginas={totalPaginas} 
+                        paginaActual={pagina} 
+                        onChange={nuevaPagina => setPagina(nuevaPagina)} />  
+
+            <ListadoGenerico listado={entidades}>
+
+             <table className="table table-stripped">
+                    {props.children(entidades!, botones)}
+             </table>
+
+             </ListadoGenerico>      
+        </>
+        
+     );
+}
+
+interface indiceEntidadProps<T>{
+    url: string;
+    urlCreacion: string;
+    children(
+        entidades: T[], botones: (
+                            urlEditar: string, 
+                            id: number) => ReactElement): ReactElement;
+    titulo: string;
+    nombreEntidad: string;
+}
+export default IndiceEntidad;
