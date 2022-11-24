3b5ec8f44a8505a95d8b1bd51945a5bf68fd2f8b
Emiliano <emiliano.nvrz@gmail.com>
2022-11-23 19:22:16 -0700

    Crear Actores

--- src/Actores/crearActores.tsx
+++ src/Actores/crearActores.tsx
@@ -1,17 +1,42 @@
-import React from "react"
+import axios from "axios"
+import React, { useState } from "react"
+import { actorCreacionDTO } from "./actores.model"
 import FormularioActores from './FormularioActores'
+import { urlActores } from '../utils/endPoints'
+
+import MostrarErrores from '../utils/MostrarErrores'
+import { useHistory } from "react-router-dom"
 
 
-function crearActores() {
+
+function CrearActores() {
+
+    const [errores, setErrores] = useState<string[]>([]);
+    const history = useHistory();
+
+    async function crear(actor:actorCreacionDTO) {
+        try{
+            await axios.post(urlActores, actor);
+            history.push('/actores');
+        }
+        catch(error){
+            setErrores(error.response.data);
+        }
+    }
+
+
     return ( 
         <>
             <h3>Crear Actores</h3>
+            <MostrarErrores errores={errores} />
             <FormularioActores 
                 modelo={{nombre: '', fechaNacimiento: undefined}} 
-                onSubmit={valores => console.log(valores)}/>  
+                onSubmit={async valores => await crear(valores)}/>  
         </>
         
      );
 }
 
-export default crearActores;
+export default CrearActores;
+
+
--- src/generos/FormularioGenero.tsx
+++ src/generos/FormularioGenero.tsx
@@ -1,4 +1,5 @@
 import { Form, Formik, FormikHelpers } from "formik"
+import React from "react";
 import { Link } from "react-router-dom"
 import * as Yup from 'yup'
 import Button from "../utils/Button"
