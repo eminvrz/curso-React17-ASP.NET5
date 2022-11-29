12c5cb9d7b007acc19a293dda8fdc3d45ef06849
Emiliano <emiliano.nvrz@gmail.com>
2022-11-28 18:45:38 -0700

    Indice Actores

--- src/Actores/indiceActores.tsx
+++ src/Actores/indiceActores.tsx
@@ -1,14 +1,41 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import IndiceEntidad from "../utils/IndiceEntidad"
+import { actorDTO } from "./actores.model"
+import { urlActores, urlGeneros } from '../utils/endPoints'
 
 function indiceActores() {
-    return ( 
+    return (
         <>
-            <h3>Indice Actores</h3>
-            <Link to="actores/crear">Crear Actor</Link>
+            <IndiceEntidad<actorDTO>
+                url={urlActores}
+                urlCreacion="actores/crear"
+                titulo="Actores"
+                nombreEntidad="Actor"
+            >
+                {(actores, botones) => <>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Nombre</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {actores?.map(actor =>
+                            <tr key={actor.id}>
+                                <td>
+                                    {botones(`actores/editar/${actor.id}`, actor.id)}
+                                </td>
+                                <td>
+                                    {actor.nombre}
+                                </td>
+                            </tr>)}
+                    </tbody>
+                </>}
+            </IndiceEntidad>
         </>
-        
-     );
+
+    )
 }
 
-export default indiceActores;
+export default indiceActores
