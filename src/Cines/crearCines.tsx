34a0855d1aacc4b9344b7a593136a0e4e850956b
Emiliano <emiliano.nvrz@gmail.com>
2022-11-30 18:19:53 -0700

    Crear Cines App

--- src/Cines/crearCines.tsx
+++ src/Cines/crearCines.tsx
@@ -1,13 +1,36 @@
+import axios from 'axios'
+import { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import { urlCines } from "../utils/endPoints"
+import MostrarErrores from '../utils/MostrarErrores'
+import { cineCreacionDTO } from './cines.model'
 import FormularioCines from "./FormularioCines"
 
-function crearCines() {
+function CrearCines() {
+
+    const history = useHistory(); // para poder navegar al indice de cines
+    const [errores, setErrores] = useState<string[]>([]);
+
+    async function crear(cine: cineCreacionDTO){
+        try{
+            await axios.post(urlCines, cine);
+            history.push('/cines');
+        }catch(error){
+            setErrores(error.response.data);
+        }
+    }
+
+
     return ( 
         <>
             <h3>Crear Cines</h3>
-            <FormularioCines modelo={{nombre: ''}} onSubmit={valores => console.log(valores)}/>
+            <MostrarErrores errores={errores}/>
+            <FormularioCines 
+                    modelo={{nombre: ''}}
+                    onSubmit={async valores => await crear(valores)}/>
         </>
         
      );
 }
 
-export default crearCines;
+export default CrearCines;
