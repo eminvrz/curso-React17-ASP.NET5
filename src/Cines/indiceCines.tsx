b510e6191dee0c54c8668819db501b7b2d04853e
Emiliano <emiliano.nvrz@gmail.com>
2022-11-30 18:23:08 -0700

    Indice Cine

--- src/Cines/indiceCines.tsx
+++ src/Cines/indiceCines.tsx
@@ -1,10 +1,40 @@
-import { Link } from "react-router-dom"
+
+import { urlCines } from "../utils/endPoints"
+import IndiceEntidad from "../utils/IndiceEntidad"
+import { cineDTO } from "./cines.model"
 
 function indiceCines() {
     return ( 
         <>
-            <h3>Indice Cines</h3>
-            <Link to="cines/crear">Crear Cine</Link>
+            <IndiceEntidad<cineDTO>
+                    url={urlCines}
+                    urlCreacion="cines/crear"
+                    titulo = "Cines"
+                    nombreEntidad = "Cine" 
+                    >
+                        {(cines, botones) => <>
+                            <thead>
+                            <tr>
+                                <th></th>
+                                <th>Nombre</th>
+                            </tr>
+                         </thead>
+                         <tbody>
+                            {cines?.map(cine => 
+                            <tr key={cine.id}>
+                                <td>
+                                    {botones(`cines/editar/${cine.id}`, cine.id)}
+                                </td>
+                                <td>
+                                    {cine.nombre}
+                                </td>
+                            </tr>)}
+                         </tbody>
+
+                        </>}
+                        
+                
+                </IndiceEntidad> 
         </>
         
      );
