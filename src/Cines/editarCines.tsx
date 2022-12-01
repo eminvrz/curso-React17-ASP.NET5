e7f14ca5ba216cc2e844dffd5cac82b36e4376f2
Emiliano <emiliano.nvrz@gmail.com>
2022-11-30 18:34:19 -0700

    Editar Cines

--- src/Cines/editarCines.tsx
+++ src/Cines/editarCines.tsx
@@ -1,13 +1,21 @@
+import EditarEntidad from "../utils/EditarEntidad"
+import { urlCines } from "../utils/endPoints"
+import { cineCreacionDTO, cineDTO } from "./cines.model"
 import FormularioCines from "./FormularioCines"
 
 function editarCines() {
     return ( 
-        <> 
-            <h3>Editar Cines</h3>
-            <FormularioCines 
-            modelo={{nombre: 'Sambil', latitud:29.101301, longitud:-110.977276}} 
-            onSubmit={valores => console.log(valores)}/>
-        </>
+        <EditarEntidad<cineCreacionDTO, cineDTO> 
+                url={urlCines}
+                urlIndice="/cines"
+                nombreEntidad="Cines"
+                >
+                    {(entidad, editar) => <FormularioCines modelo={entidad}
+                        onSubmit={ async valores  => {
+                        await editar(valores)
+                    }}
+                /> }
+                </EditarEntidad>
         
      );
 }
