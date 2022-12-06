174876bc789f901a43447d53d2e39324039152e4
Emiliano <emiliano.nvrz@gmail.com>
2022-12-06 15:10:42 -0700

    Borrar Peliculas

--- src/Peliculas/LandingPage.tsx
+++ src/Peliculas/LandingPage.tsx
@@ -1,28 +1,35 @@
-import axios from "axios"
+import axios, { AxiosResponse } from "axios"
 import { useEffect, useState } from "react"
+import AlertaContext from "../utils/AlertaContext"
+import { urlPeliculas } from '../utils/endPoints'
 import ListadoPeliculas from "./listadoPeliculas"
 import { landingPageDTO } from './peliculas.model'
-import { urlPeliculas } from '../utils/endPoints'
-import { AxiosResponse } from 'axios'
 
 function LandingPage() {
 
     const [peliculas, setPeliculas] = useState<landingPageDTO>({})
 
     useEffect(() => {
+        cargarDatos();
+    }, [])
+
+    function cargarDatos() {
         axios.get(urlPeliculas)
         .then((respuesta: AxiosResponse<landingPageDTO>) => {
             setPeliculas(respuesta.data);
         })
-    }, [])
+    }
 
     return (
         <>
-            <h3>En Cartelera</h3>
-            <ListadoPeliculas peliculas={peliculas.enCines} />
+            <AlertaContext.Provider value={() => cargarDatos()}>
+                <h3>En Cartelera</h3>
+                <ListadoPeliculas peliculas={peliculas.enCines} />
 
-            <h3>Proximos Estrenos</h3>
-            <ListadoPeliculas peliculas={peliculas.proximosEstrenos} />
+                <h3>Proximos Estrenos</h3>
+                <ListadoPeliculas peliculas={peliculas.proximosEstrenos} />
+            </AlertaContext.Provider>
+            
         </>
     )
 }
--- src/Peliculas/peliculaIndividual.tsx
+++ src/Peliculas/peliculaIndividual.tsx
@@ -1,13 +1,26 @@
 
+import axios from "axios"
 import { Link } from "react-router-dom"
 import Button from "../utils/Button"
+import confirmar from "../utils/Confirmar"
+import { urlPeliculas } from "../utils/endPoints"
 import { peliculaDTO } from "./peliculas.model"
 import css from './peliculasIndividual.module.css'
+import { useContext } from 'react'
+import AlertaContext from '../utils/AlertaContext'
 
 
 function PeliculaIndividual(props: peliculaIndividualProps) {
 
     const construirLink = () => `/pelicula/${props.pelicula.id}`
+    const alerta = useContext(AlertaContext)
+
+    function borrarPelicula(){
+        axios.delete(`${urlPeliculas}/${props.pelicula.id}`)
+            .then(() => {
+                alerta();
+            })
+    }
 
     return (  
         <div className={css.div}>
@@ -20,7 +33,9 @@ function PeliculaIndividual(props: peliculaIndividualProps) {
             <div>
                 <Link style={{marginRight: '1rem'}}  className="btn btn-info"
                     to={`/peliculas/editar/${props.pelicula.id}`}>Editar</Link>
-                <Button className="btn btn-danger">Borrar</Button>
+                <Button 
+                onClick={() => confirmar(() => borrarPelicula())}
+                className="btn btn-danger">Borrar</Button>
             </div>
         </div>
     );
