477725f54035418a4b3c4a570bb5028933e5af8f
Emiliano <emiliano.nvrz@gmail.com>
2022-12-07 16:04:01 -0700

    Filtro de Peliculas p2

--- src/Peliculas/filtroPeliculas.tsx
+++ src/Peliculas/filtroPeliculas.tsx
@@ -7,6 +7,9 @@ import axios from "axios"
 import { AxiosResponse } from 'axios'
 import { peliculaDTO } from './peliculas.model'
 import ListadoPeliculas from "./listadoPeliculas"
+import { useHistory, useLocation } from 'react-router-dom'
+import { hostname } from "os"
+import Paginacion from "../utils/Paginacion"
 
 function FiltroPeliculas() {
 
@@ -21,6 +24,10 @@ function FiltroPeliculas() {
 
     const [generos, setGeneros] = useState<generoDTO[]>([]);
     const [peliculas, setPeliculas] = useState<peliculaDTO[]>([]);
+    const history = useHistory();
+    const query = new URLSearchParams(useLocation().search);
+    const [totalPaginas, setTotalPaginas] = useState(0);
+
 
     useEffect(() => {
         axios.get(`${urlGeneros}/todos`)
@@ -30,15 +37,66 @@ function FiltroPeliculas() {
     }, [])
 
     useEffect(() => {
+
+        if(query.get('titulo')){
+            valorInicial.titulo = query.get('titulo')!;
+        }
+
+        if(query.get('generoId')){
+            valorInicial.generoId = parseInt(query.get('generoId')!, 10);
+        }
+
+        if(query.get('proximosEstrenos')){
+            valorInicial.proximosEstrenos = true;
+        }
+
+        if(query.get('enCines')){
+            valorInicial.enCines = true;
+        }
+
+        if(query.get('pagina')){
+            valorInicial.pagina = parseInt(query.get('pagina')!, 10);
+        }
+
+
+
         buscarPeliculas(valorInicial);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
 
     function buscarPeliculas(valores: filtroPeliculasForm){
+        modificarURL(valores);
         axios.get(`${urlPeliculas}/filtrar`, {params: valores})
                 .then((respuesta: AxiosResponse<peliculaDTO[]>) => {
+                    const totalRegistro = 
+                    parseInt(respuesta.headers['cantidadtotalregistros'], 10);
+                    setTotalPaginas(Math.ceil(totalRegistro/valorInicial.recordsPorPagina));
                     setPeliculas(respuesta.data);
                 })
+    }
+
+    function modificarURL(valores: filtroPeliculasForm){
+        const queryStrings: string[] = [];
+        if(valores.titulo){
+            queryStrings.push(`titulo=${valores.titulo}`);
+        }
+
+        if(valores.generoId != 0){
+            queryStrings.push(`generoId=${valores.generoId}`);
+        }
+
+        if(valores.proximosEstrenos){
+            queryStrings.push(`proximosEstrenos=${valores.proximosEstrenos}`);
+        }
+
+        if(valores.enCines){
+            queryStrings.push(`enCines=${valores.enCines}`);
+        }
+
+        queryStrings.push(`pagina=${valores.pagina}`);
+        // titulo=felipe&pagina=3&... ira separando los distintos elementos del arreglo
+        history.push(`/pelicula/filtrar?${queryStrings.join('&')}`)
     }
 
     return ( 
@@ -46,10 +104,14 @@ function FiltroPeliculas() {
             <h3>Filtro Peliculas</h3>
 
             <Formik initialValues={valorInicial}
-                    onSubmit ={valores => buscarPeliculas(valores)}
+                    onSubmit ={valores => {
+                        valores.pagina = 1;
+                        buscarPeliculas(valores)
+                    }}
             >
                 {(formikProps) => (
-                    <Form>
+                    <>
+                        <Form>
                         <div className="form-inline">
                             <div className="form-group mb-2">
                                 <label htmlFor="titulo" className="sr-only">Titulo</label>
@@ -95,11 +157,18 @@ function FiltroPeliculas() {
                                 >Limpiar
                             </Button>
                         </div>
-                    </Form>
-                )}
-            </Formik>
+                        </Form>
+                        <ListadoPeliculas peliculas={peliculas}/>
+                        <Paginacion cantidadTotalPaginas={totalPaginas} 
+                                 paginaActual={formikProps.values.pagina} 
+                                 onChange={nuevaPagina => {
+                                    formikProps.values.pagina = nuevaPagina;
+                                    buscarPeliculas(formikProps.values);
 
-            <ListadoPeliculas peliculas={peliculas}/>
+                                 }} />  
+                    </> 
+                )} 
+            </Formik>
         </>
         
      );
